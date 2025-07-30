cada267728797eda0d9a736931179cfe9986ca59
Sanjana432 <sanjana.sundar.k@gmail.com>
2025-07-30 20:25:56 +0530

    Create Features.js

---
+++ src/components/Features.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Features() {
+  return (
+    <section className="features">
+      <h2>How It Works</h2>
+      <div className="feature-grid">
+        <div className="feature">
+          <div className="placeholder-img"></div>
+          <p>Upload your room photo</p>
+        </div>
+        <div className="feature">
+          <div className="placeholder-img"></div>
+          <p>Pick a theme</p>
+        </div>
+        <div className="feature">
+          <div className="placeholder-img"></div>
+          <p>Get AI suggestions</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
