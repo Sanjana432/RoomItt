d8a5178a7f400ee828d114c7e0c0586eab51a07b
Sanjana432 <sanjana.sundar.k@gmail.com>
2025-07-30 20:25:19 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Inspiration from './components/Inspiration';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Inspiration />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
