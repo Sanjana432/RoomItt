a1dd4c99f7f19173b6efc440376f159b4bd313dc
Sanjana432 <sanjana.sundar.k@gmail.com>
2025-07-30 20:26:12 +0530

    Create Inspiration.js

---
+++ src/components/Inspiration.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Inspiration() {
+  return (
+    <section className="inspiration">
+      <h2>Design Inspirations</h2>
+      <div className="inspo-grid">
+        <div className="placeholder-img"></div>
+        <div className="placeholder-img"></div>
+        <div className="placeholder-img"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Inspiration;
